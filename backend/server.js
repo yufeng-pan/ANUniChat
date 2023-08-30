65e5ef0627f7c0b0bbba607395dae155512d6b36
Yufeng Pan <yufeng.pan.0001@gmail.com>
2023-08-31 00:03:23 +1000

    setup basic backend and frontend files

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const chats = require("./data/data");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((chat) => {
+    return chat._id === req.params.id;
+  });
+
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

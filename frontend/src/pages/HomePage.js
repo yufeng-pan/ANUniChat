b5406154bb0725bbd6a6f84b4d29de5898cfd31a
Yufeng Pan <yufeng.pan.0001@gmail.com>
2023-08-31 17:21:29 +1000

    add basic structure for HomePage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,57 @@
 import React from "react";
+import {
+  Box,
+  Container,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work Sans" color="black">
+          ANUniChat
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth="1px"
+        color="black"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;

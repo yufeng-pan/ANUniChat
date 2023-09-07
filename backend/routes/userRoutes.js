513b29bfe701143980ad1aaa57a975b5c1232615
Yufeng Pan <yufeng.pan.0001@gmail.com>
2023-09-08 00:06:33 +1000

    complete users search that does not include the current user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
-const { signupUser, authUser } = require("../controllers/userController");
+const {
+  signupUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
 
 const express = require("express");
 const router = express.Router();
 
-router.route("/").post(signupUser);
+router.route("/").post(signupUser).get(protect, allUsers);
+
 router.post("/signin", authUser);
 
 module.exports = router;

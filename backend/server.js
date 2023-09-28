5f7b32bad3978cf8d79aeb035b174945d2011eb3
Yufeng Pan <yufeng.pan.0001@gmail.com>
2023-09-28 15:16:15 +1000

    setup files for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -13,13 +14,23 @@ connectDB();
 app.use("/public", express.static("public"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ------------Deployment----------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+// ------------Deployment----------------
 
 app.use(notFound);
 app.use(errorHandler);

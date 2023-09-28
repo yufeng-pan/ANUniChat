7682a4aab4bafbcccf8fd698d9b73d3970805ec3
Yufeng Pan <yufeng.pan.0001@gmail.com>
2023-09-28 16:40:14 +1000

    Revert "try to fix the socket.io issue"
    This reverts commit 24b682d430f5f2f50d59cf777e92797ceee291a7.

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://anunichat.onrender.com",
+    origin: "http://localhost:3000",
   },
 });
 
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://anunichat.onrender.com",
+      target: "http://localhost:4000",
       changeOrigin: true,
     })
   );
